3aa915bdaf8e53117c81154775582dfe88b9783c
Priyotosh <cts-btp@CTS-BTPs-Mac-mini.local>
2014-07-11 17:07:40 +0530

    test coomit

--- lib.js
+++ lib.js
@@ -5,12 +5,10 @@ var test = function(message){
 var add = function(a,b){
 	return a+b;
 };
-var sub = function(a,b){
-	return a-b;
-};
-var pro = function(a,b){
-	return a*b;
-};
-var div = function(a,b){
-	return a/b;
-};
+
+var mul=function(a,b){
+
+return a*b;
+}
+
+SampleTest.test.js
