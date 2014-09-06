a302a852d26f50acb8d6d87e4e0f3272a732fef8
CTSMacBookPro <PriyotoshKumar.Ghosh@cognizant.com>
2014-09-06 21:29:54 +0530

    test added

--- lib.js
+++ lib.js
@@ -6,20 +6,18 @@ var add = function(a,b){
 	return a+b;
 };
 var sub = function(a,b){
-	return a-b;
-};
-<<<<<<< HEAD
-	return a*b;
-};
+ 	return a-b;
+ };
 
-var div=function(){
 
-return a/b;
-
-};
-
-=======
+ var div=function(){
+ 
+ return a/b;
+ 
+ };
+ 
 var pro = function(a,b){
 	return a*b;
 };
->>>>>>> Product Added
+
+
