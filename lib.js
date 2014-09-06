7b7e0f0758a156919481269be442ee8a9d90bbf0
VishwasHK <vishwa.vijay143@gmail.com>
2014-09-06 20:51:53 +0530

    Revert "Mul Added" This reverts commit bee3b1d97aa582d8f86a28f0d29363a79186a94b.
    Conflicts:
    	lib.js

--- lib.js
+++ lib.js
@@ -6,17 +6,3 @@ var add = function(a,b){
 	return a+b;
 };
 
-var mul=function(a,b){
-
-return a*b;
-};
-var sayHello = function(argument) {
-	alert(argument);
-};
-var sayHelloTeam = function(){
-	alert('Hi Team');
-};
-
-var test1 = function(){
-	alert('Sample test');
-}
