18c631ae3231ee249b388d80bac5daa2a911831f
CTSMacBookPro <PriyotoshKumar.Ghosh@cognizant.com>
2014-09-06 21:34:18 +0530

    Priyotosh's change

--- lib.js
+++ lib.js
@@ -6,18 +6,13 @@ var add = function(a,b){
 	return a+b;
 };
 var sub = function(a,b){
- 	return a-b;
- };
-
-
- var div=function(){
- 
- return a/b;
- 
- };
- 
+	return a-b;
+};
 var pro = function(a,b){
 	return a*b;
 };
-
-
+var div=function(){
+ 
+return a/b;
+ 
+};
